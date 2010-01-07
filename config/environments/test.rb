99e23290f588d71158ca3657c5addf9d917897a9
Jeff Whitmire <jeff@jwhitmire.com>
2010-01-07 11:26:53 -0500

    Moving config.gems stuff back into environment.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,9 +27,3 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem "rspec-rails", :lib => 'spec/rails'
-config.gem "rspec", :lib => 'spec'
-config.gem "factory_girl"
-config.gem "cucumber", :version => ">= 0.6.1"
-config.gem "cucumber-rails", :version => ">= 0.2.3"
-config.gem "capybara"
