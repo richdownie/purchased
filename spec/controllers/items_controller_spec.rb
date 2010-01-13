ef115ab978c376ec55cb7f85c5a23d57180b0fbe
Rich Downie <rich@richdownie.com>
2010-01-12 22:17:03 -0500

    more tests

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,10 +1,20 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ItemsController do
+  describe "index" do
+    integrate_views
+    context "when logged in a a user" do
+      before do
+        @user = Factory.build(:user)
+        
+        get :index
+      end
 
-  #Delete this example and add some real ones
-  it "should use ItemsController" do
-    controller.should be_an_instance_of(ItemsController)
+      it "should be successful" do
+        response.should be_success
+        response.should render_template('index')
+        response.should have_text(/Items/)
+      end
+    end
   end
-
 end
