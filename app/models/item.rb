23e725b749c285ceffd5523d528ff94a8da25733
Rich Downie <rich@richdownie.com>
2010-02-04 18:50:49 -0500

    removing rest of items

--- app/models/item.rb
+++
@@ -1,5 +0,0 @@
-class Item < ActiveRecord::Base
-  validates_presence_of :name
-
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
-  map.resources :items
   map.resources :user_sessions
   map.resources :users
 
